692dbd906c09f908849c4319e0fe8cc81a4da848
aaron93 <memoji1236@gmail.com>
2020-11-10 22:56:39 +0900

    3.2

--- users/models.py
+++ users/models.py
@@ -6,4 +6,20 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User Model """
+
+    # Preference selection
+    PREFERENCE_BOOK = "Books"
+    PREFERENCE_MOVIE = "Movies"
+    PREFERENCE_CHOICES = ((PREFERENCE_BOOK, "Books"), (PREFERENCE_MOVIE, "Movies"))
+
+    # Language selection
+    LANGUAGE_KR = "korean"
+    LANGUAGE_EN = "english"
+    LANGUAGE_CHOICES = ((LANGUAGE_KR, "Korean"), (LANGUAGE_EN, "English"))
+
+    avatar = models.ImageField(null=True, blank=True)
+    Preference = models.CharField(
+        choices=PREFERENCE_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
